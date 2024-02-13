9a359895c35a8625a14c90366c1165872b0fc3e8
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2024-02-13 18:34:45 -0500

    Remove block library

--- functions.php
+++ functions.php
@@ -233,4 +233,11 @@ function cp_race_new_excerpt_more( $more ) {
 }
 add_filter('excerpt_more', 'cp_race_new_excerpt_more');
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+ } 
+ add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 ?>
